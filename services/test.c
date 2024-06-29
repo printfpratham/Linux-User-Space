8ed9550a894fb5b9a6c222db05a4a2d2e8e9963d
pratham.malaviya <pratham.malaviya@siliconsignals.io>
2024-06-29 18:20:59 +0530

    Services
    Signed-off-by: pratham.malaviya <pratham.malaviya@siliconsignals.io>

---
+++ services/test.c
@@ -0,0 +1,12 @@
+#include  <stdio.h>
+
+
+int main()
+{
+	int i=0;
+	while(1){
+	printf("Hello world\n");
+	sleep(1);
+	}
+	return 0;
+}
---
+++ services/test2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+	while(1){
+		printf("Service will run after 10 sec\n");
+	}
+	return 0;
+}
